01f444af53a69c5da282e9cfbc93c41fd773e67a
Amirul Syakirin <amsyakir@gmail.com>
2024-06-19 22:25:48 +0800

    Add files via upload

---
+++ 3_treasure_island_c.py
@@ -0,0 +1,46 @@
+treasure = ('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+a = input('You\'re in a junction. Where do you want to go? Type "left" or "right".\n').lower()
+if a == "left":
+          b = input('You\'re by the seaside and you saw an island not far away. Type "swim" if you want to swim to the island or type "wait" to wait for a boat to the island.\n').lower()
+          if b == "wait":
+                    c = input('You\'ve arrived on the the island and you stumbled upon three doors. Red, yellow and blue. Which one do you choose?\n').lower()
+                    if c == "yellow":
+                              print("\n\nCongratulations! You found the treasure.")
+                              print(treasure)
+                    elif c == "blue":
+                              print("You were eaten by beasts. Game Over.")
+                    elif c == "red":
+                              print("You were burned by fire. Game Over.")
+                    else:
+                              print("This is an unknown door. Game Over.")
+          else:
+                    print("You were attacked by trouts. Game Over.")
+else:
+          print("You fell into a hole. Game Over.")
